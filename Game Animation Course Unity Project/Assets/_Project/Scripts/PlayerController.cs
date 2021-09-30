4a6ba91842e64c68e5d21d5392b8d865a7668b0d
Matthew Brelsford <matt@giantlightstudios.com>
2021-09-30 10:31:11 -0400

    Auto formatting player controller

--- Game Animation Course Unity Project/Assets/_Project/Scripts/PlayerController.cs
+++ Game Animation Course Unity Project/Assets/_Project/Scripts/PlayerController.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 using Prime31;
 using Prime31.StateKitLite;
 
-public enum PlayerMovementStates {
-    None, Idle, Walking, Jumping, Falling, Hurt, Dead
+public enum PlayerMovementStates
+{
+	None,
+	Idle,
+	Walking,
+	Jumping,
+	Falling,
+	Hurt,
+	Dead
 }
 
 [RequireComponent(typeof(CharacterController2D))]
 [RequireComponent(typeof(Actor))]
 public class PlayerController : StateKitLite<PlayerMovementStates>
 {
-    [Header("Misc References")]
-    public CharacterController2D characterController2d;
-    private Actor actor;
+	[Header("Misc References")]
+	public CharacterController2D characterController2d;
+	private Actor actor;
 
-    [Header("Input")]
-    public Vector2 movementInput;
-    public bool jumpInput;
-  	PlayerInputActions playerInputActions;
+	[Header("Input")]
+	public Vector2 movementInput;
+	public bool jumpInput;
+	PlayerInputActions playerInputActions;
 
-    [Header("Movement")]
+	[Header("Movement")]
 	public float Gravity = 20f;
 	public float MovementSpeed = 10;
 	public float Deacceleration = .1f;
 	public float DeaccelerationVol = 0f;
-    public Vector2 velocity;
+	public Vector2 velocity;
 
 	public float UncontrollableDeacceleration = .1f;
 	public float UncontrollableDeaccelerationVol = 0f;
@@ -35,238 +42,279 @@ public class PlayerController : StateKitLite<PlayerMovementStates>
 	public float JumpSpeed = 1;
 	public bool jump_clear = true;
 
-    [Header("Stats")]
-    public int HP;
-    bool isDead = false;
+	[Header("Stats")]
+	public int HP;
+	bool isDead = false;
 
-    [Header("Art")]
-    public Transform characterBody;
+	[Header("Art")]
+	public Transform characterBody;
 
-    [Header("Animation")]
-    public Animator animator;
-    public string speedProperty = "speed";
-    public string measuredVelocityProperty = "measuredVelocity";
-    public string verticalSpeedProperty = "vertical";
-    public string groundedProperty = "grounded";
-    public string deadProperty = "dead";
+	[Header("Animation")]
+	public Animator animator;
+	public string speedProperty = "speed";
+	public string measuredVelocityProperty = "measuredVelocity";
+	public string verticalSpeedProperty = "vertical";
+	public string groundedProperty = "grounded";
+	public string deadProperty = "dead";
 
+	protected override void Awake()
+	{
+		base.Awake();
 
-    protected override void Awake(){
-        base.Awake();
+		actor = GetComponent<Actor>();
+		actor.OnDie += HandleDeath;
 
-        actor = GetComponent<Actor>();
-        actor.OnDie += HandleDeath;
-
-        characterController2d = GetComponent<CharacterController2D>();
+		characterController2d = GetComponent<CharacterController2D>();
 
 		playerInputActions = new PlayerInputActions();
 		playerInputActions.Player.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
 		playerInputActions.Player.Jump.started += ctx => jumpInput = true;
 		playerInputActions.Player.Jump.canceled += ctx => jumpInput = false;
-    }
-
-    public void Start(){
-        initialState = PlayerMovementStates.Idle;
-    }
-
-    
-    protected override void Update(){
-        base.Update();
-
-        if(velocity.x > 0 && characterBody.eulerAngles.y != 0){
-            characterBody.eulerAngles = Vector3.up * 0;
-        }else if(velocity.x < 0 && characterBody.eulerAngles.y != 180){
-            characterBody.eulerAngles = Vector3.up * 180;
-        }
-
-        if(characterController2d.isGrounded && velocity.y < 0){
-            velocity.y = -.1f;
-        }
-
-        animator.SetFloat(speedProperty, Mathf.Abs(movementInput.x));
-        animator.SetFloat(measuredVelocityProperty, Mathf.Abs(velocity.x));
-        animator.SetFloat(verticalSpeedProperty, velocity.y);
-        animator.SetBool(groundedProperty, characterController2d.isGrounded);
-    }
-
-	private void OnEnable(){
-        if(playerInputActions != null){
-		    playerInputActions.Enable();
-        }
 	}
 
-	private void OnDisable(){
-        if(playerInputActions != null){
-    		playerInputActions.Disable();
-        }
+	public void Start()
+	{
+		initialState = PlayerMovementStates.Idle;
 	}
 
-    // StateKitLite States
+	protected override void Update()
+	{
+		base.Update();
+
+		if (velocity.x > 0 && characterBody.eulerAngles.y != 0)
+		{
+			characterBody.eulerAngles = Vector3.up * 0;
+		}
+		else if (velocity.x < 0 && characterBody.eulerAngles.y != 180)
+		{
+			characterBody.eulerAngles = Vector3.up * 180;
+		}
+
+		if (characterController2d.isGrounded && velocity.y < 0)
+		{
+			velocity.y = -.1f;
+		}
+
+		animator.SetFloat(speedProperty, Mathf.Abs(movementInput.x));
+		animator.SetFloat(measuredVelocityProperty, Mathf.Abs(velocity.x));
+		animator.SetFloat(verticalSpeedProperty, velocity.y);
+		animator.SetBool(groundedProperty, characterController2d.isGrounded);
+	}
+
+	private void OnEnable()
+	{
+		if (playerInputActions != null)
+		{
+			playerInputActions.Enable();
+		}
+	}
+
+	private void OnDisable()
+	{
+		if (playerInputActions != null)
+		{
+			playerInputActions.Disable();
+		}
+	}
+
+	// StateKitLite States
 	void None_Enter() {}
 	void None_Tick() {}
 	void None_Exit() {}
 
-    // Idle
+	// Idle
 	void Idle_Enter() {}
-	void Idle_Tick() {
+	void Idle_Tick()
+	{
 
-        ApplyGravity();
-        DoVelocityMovement();
+		ApplyGravity();
+		DoVelocityMovement();
 
-        if(CheckForJump()){
-            return;
-        }
-        if(CheckForFall()){
-            return;
-        }
+		if (CheckForJump())
+		{
+			return;
+		}
+		if (CheckForFall())
+		{
+			return;
+		}
 
-        HandleXMovement();
-        if(Mathf.Abs(movementInput.x) > 0){
-            currentState = PlayerMovementStates.Walking;
-            return;
-        }
+		HandleXMovement();
+		if (Mathf.Abs(movementInput.x) > 0)
+		{
+			currentState = PlayerMovementStates.Walking;
+			return;
+		}
 
-    }
+	}
 	void Idle_Exit() {}
 
-    // Walking
+	// Walking
 	void Walking_Enter() {}
-	void Walking_Tick() {
+	void Walking_Tick()
+	{
 
-        ApplyGravity();
-        DoVelocityMovement();
+		ApplyGravity();
+		DoVelocityMovement();
 
-        if(CheckForJump()){
-            return;
-        }
-        if(CheckForFall()){
-            return;
-        }
+		if (CheckForJump())
+		{
+			return;
+		}
+		if (CheckForFall())
+		{
+			return;
+		}
 
-        HandleXMovement();
-        if(movementInput.x == 0){
-            currentState = PlayerMovementStates.Idle;
-            return;
-        }
-    }
+		HandleXMovement();
+		if (movementInput.x == 0)
+		{
+			currentState = PlayerMovementStates.Idle;
+			return;
+		}
+	}
 	void Walking_Exit() {}
 
-    // Jumping
-	void Jumping_Enter() {
-        velocity.y = JumpSpeed;
-    }
+	// Jumping
+	void Jumping_Enter()
+	{
+		velocity.y = JumpSpeed;
+	}
 
-	void Jumping_Tick() {
+	void Jumping_Tick()
+	{
 
-        if(!jumpInput){
-            velocity.y = 0;
-            currentState = PlayerMovementStates.Falling;
-            return;
-        }
+		if (!jumpInput)
+		{
+			velocity.y = 0;
+			currentState = PlayerMovementStates.Falling;
+			return;
+		}
 
-        if(velocity.y <= 0){
-            currentState = PlayerMovementStates.Falling;
-            return;
-        }
+		if (velocity.y <= 0)
+		{
+			currentState = PlayerMovementStates.Falling;
+			return;
+		}
 
-        ApplyGravity();
-        HandleXMovement();
-        DoVelocityMovement();
-    }
+		ApplyGravity();
+		HandleXMovement();
+		DoVelocityMovement();
+	}
 	void Jumping_Exit() {}
 
-    // Falling
-	void Falling_Enter() {
-        if(velocity.y < 0){
-            velocity.y = 0;
-        }
-    }
-
-	void Falling_Tick() {
-
-        // if hits head, start falling
-        if(velocity.y > 0 && characterController2d.collisionState.above){
+	// Falling
+	void Falling_Enter()
+	{
+		if (velocity.y < 0)
+		{
 			velocity.y = 0;
-        }
+		}
+	}
 
-        if(characterController2d.isGrounded){
-            currentState = PlayerMovementStates.Idle;
-            return;
-        }
+	void Falling_Tick()
+	{
 
-        ApplyGravity();
-        HandleXMovement();
-        DoVelocityMovement();
+		// if hits head, start falling
+		if (velocity.y > 0 && characterController2d.collisionState.above)
+		{
+			velocity.y = 0;
+		}
 
-    }
+		if (characterController2d.isGrounded)
+		{
+			currentState = PlayerMovementStates.Idle;
+			return;
+		}
+
+		ApplyGravity();
+		HandleXMovement();
+		DoVelocityMovement();
+
+	}
 	void Falling_Exit() {}
 
-    // Hurt
+	// Hurt
 	void Hurt_Enter() {}
-	
-    void Hurt_Tick() {
-        DoVelocityMovement();
-        ApplyGravity();
-    }
-	
-    void Hurt_Exit() {}
 
-    // Dead
+	void Hurt_Tick()
+	{
+		DoVelocityMovement();
+		ApplyGravity();
+	}
+
+	void Hurt_Exit() {}
+
+	// Dead
 	void Dead_Enter() {}
-	
-    void Dead_Tick() {
-        DoVelocityMovement();
-        ApplyGravity();
-    }
-	
-    void Dead_Exit() {}
 
-    public bool CheckForJump(){
-        if(jumpInput){
-            currentState = PlayerMovementStates.Jumping;
-            return true;
-        }
-        return false;
-    }
+	void Dead_Tick()
+	{
+		DoVelocityMovement();
+		ApplyGravity();
+	}
 
-    public bool CheckForFall(){
-        // if not grounded, fall
-        if(!characterController2d.isGrounded){
-            currentState = PlayerMovementStates.Falling;
-            return true;
-        }
-        return false;
-    }
+	void Dead_Exit() {}
 
-    public void ApplyGravity(){
-        velocity.y -= Gravity * Time.deltaTime;
-    }
+	public bool CheckForJump()
+	{
+		if (jumpInput)
+		{
+			currentState = PlayerMovementStates.Jumping;
+			return true;
+		}
+		return false;
+	}
 
-    public void HandleGroundedVerticalVelocity(){
-        if(characterController2d.isGrounded){
-            velocity.y = -.01f;
-        }
-    }
+	public bool CheckForFall()
+	{
+		// if not grounded, fall
+		if (!characterController2d.isGrounded)
+		{
+			currentState = PlayerMovementStates.Falling;
+			return true;
+		}
+		return false;
+	}
 
-    Vector3 _uncontrollableVelocity;
-	private void HandleXMovement(){
-		if(movementInput.x == 0){
+	public void ApplyGravity()
+	{
+		velocity.y -= Gravity * Time.deltaTime;
+	}
+
+	public void HandleGroundedVerticalVelocity()
+	{
+		if (characterController2d.isGrounded)
+		{
+			velocity.y = -.01f;
+		}
+	}
+
+	Vector3 _uncontrollableVelocity;
+	private void HandleXMovement()
+	{
+		if (movementInput.x == 0)
+		{
 			velocity.x = Mathf.SmoothDamp(velocity.x, 0 + _uncontrollableVelocity.x, ref DeaccelerationVol, Deacceleration);
-		}else{
+		}
+		else
+		{
 			velocity.x = Mathf.SmoothDamp(velocity.x, (movementInput.x * MovementSpeed) + _uncontrollableVelocity.x, ref DeaccelerationVol, Deacceleration);
 		}
 		_uncontrollableVelocity.x = Mathf.SmoothDamp(_uncontrollableVelocity.x, 0, ref UncontrollableDeaccelerationVol, UncontrollableDeacceleration);
 	}
 
-    public void DoVelocityMovement(){
-        characterController2d.Move(velocity * Time.deltaTime);
-    }
+	public void DoVelocityMovement()
+	{
+		characterController2d.Move(velocity * Time.deltaTime);
+	}
 
-    public void HandleDeath(){
-        velocity = Vector2.zero;
-        currentState = PlayerMovementStates.Dead;
-        animator.SetTrigger(deadProperty);
-        GameManager.Instance.HandleLose();
-    }
+	public void HandleDeath()
+	{
+		velocity = Vector2.zero;
+		currentState = PlayerMovementStates.Dead;
+		animator.SetTrigger(deadProperty);
+		GameManager.Instance.HandleLose();
+	}
 
 }
