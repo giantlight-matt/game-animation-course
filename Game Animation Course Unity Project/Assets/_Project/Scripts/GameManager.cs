87ffb9511835b8c43ebdc1fdea5e3849790bf588
Matthew Brelsford <matt@giantlightstudios.com>
2021-09-29 20:25:27 -0400

    Collectibles. Pause menu with restart/quit buttons

--- Game Animation Course Unity Project/Assets/_Project/Scripts/GameManager.cs
+++ Game Animation Course Unity Project/Assets/_Project/Scripts/GameManager.cs
@@ -1,53 +1,138 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
-    public IEnumerator Start(){
-        FXManager.Instance.JumpToBlack();
-        yield return new WaitForSeconds(1f);
-        FXManager.Instance.FadeIn(1f);
-    }
+	public Text scoreText;
+	public CanvasGroup pauseUI;
 
-    public void TransitionToScene(string newSceneName){
-        StartCoroutine(TransitionToSceneCoroutine(newSceneName));
-    }
+	private int score = 0;
+	private bool isPauseVisible;
 
-    public IEnumerator TransitionToSceneCoroutine(string newSceneName){
-        float transitionDuration = 1f;
+	public IEnumerator Start()
+	{
+		UpdateScoreUI();
+		UpdatePauseVisibility();
 
-        FXManager.Instance.FadeOut(transitionDuration);
-        yield return new WaitForSeconds(transitionDuration);
+		FXManager.Instance.JumpToBlack();
+		yield return null;
+		FXManager.Instance.FadeIn(1f);
+	}
 
-        yield return SceneManager.LoadSceneAsync(newSceneName, LoadSceneMode.Single);
-    }
+	public void Update()
+	{
+		if (Keyboard.current.escapeKey.wasPressedThisFrame)
+		{
+			TogglePause();
+		}
 
-    public void HandleWin(string nextSceneName){
-        StartCoroutine(HandleWinCoroutine(nextSceneName));
-    }
+		if (Keyboard.current.rKey.wasPressedThisFrame)
+		{
+			RestartCurrentSceneImmediately();
+		}
+	}
 
-    public IEnumerator HandleWinCoroutine(string nextSceneName){
-        yield return new WaitForSeconds(2f);
-        // reload to current scene
-        if(string.IsNullOrEmpty(nextSceneName)){
-            TransitionToScene(SceneManager.GetActiveScene().name);
-        }else{
-            TransitionToScene(nextSceneName);
-        }
-    }
+	public void TransitionToScene(string newSceneName, float duration)
+	{
+		StartCoroutine(TransitionToSceneCoroutine(newSceneName, duration));
+	}
 
-    public void HandleLose(){
-        StartCoroutine(HandleLoseCoroutine());
-    }
+	public IEnumerator TransitionToSceneCoroutine(string newSceneName, float duration)
+	{
 
-    public IEnumerator HandleLoseCoroutine(){
-        yield return new WaitForSeconds(5f);
-        // reload to current scene
-        TransitionToScene(SceneManager.GetActiveScene().name);
+		FXManager.Instance.FadeOut(duration);
+		yield return new WaitForSeconds(duration);
 
-    }
+		yield return SceneManager.LoadSceneAsync(newSceneName, LoadSceneMode.Single);
+	}
 
+	public void HandleWin(string nextSceneName)
+	{
+		StartCoroutine(HandleWinCoroutine(nextSceneName));
+	}
+
+	public IEnumerator HandleWinCoroutine(string nextSceneName)
+	{
+		yield return new WaitForSeconds(2f);
+		// reload to current scene
+		if (string.IsNullOrEmpty(nextSceneName))
+		{
+			TransitionToScene(SceneManager.GetActiveScene().name, 1f);
+		}
+		else
+		{
+			TransitionToScene(nextSceneName, 1f);
+		}
+	}
+
+	public void HandleLose()
+	{
+		StartCoroutine(HandleLoseCoroutine());
+	}
+
+	public IEnumerator HandleLoseCoroutine()
+	{
+		yield return new WaitForSeconds(5f);
+		// reload to current scene
+		RestartCurrentScene(1f);
+	}
+
+	public void RestartCurrentSceneImmediately()
+	{
+		RestartCurrentScene(0);
+	}
+
+	public void RestartCurrentScene(float duration)
+	{
+		TransitionToScene(SceneManager.GetActiveScene().name, duration);
+	}
+
+	public void GetPoints(int amount)
+	{
+		score += amount;
+		UpdateScoreUI();
+	}
+
+	public void UpdateScoreUI()
+	{
+		scoreText.text = score.ToString();
+	}
+
+	public void TogglePause()
+	{
+		isPauseVisible = !isPauseVisible;
+		UpdatePauseVisibility();
+	}
+
+	public void UpdatePauseVisibility()
+	{
+		if (isPauseVisible)
+		{
+			pauseUI.alpha = 1;
+			pauseUI.gameObject.SetActive(true);
+			pauseUI.blocksRaycasts = true;
+			pauseUI.interactable = true;
+		}
+		else
+		{
+			pauseUI.alpha = 0;
+			pauseUI.gameObject.SetActive(false);
+			pauseUI.blocksRaycasts = false;
+			pauseUI.interactable = false;
+		}
+	}
+
+	public void QuitGame()
+	{
+		#if UNITY_EDITOR
+		Debug.LogError("Application quitting doesn't work in the editor, but should work in desktop builds");
+		#endif
+
+		Application.Quit();
+	}
 
 }
